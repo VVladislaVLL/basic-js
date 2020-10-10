e2d23c557fbe1a259c3dfdb12c0f8e40d0b0b9c8
VladKoch_ <vlad2002kochurko@gmail.com>
2020-10-10 16:55:23 +0300

    feat: implement function calculateDeoth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr) {
+    let subarraysCounter = 0;
+    for (let subarray of arr) {
+      if (Array.isArray(subarray)) {
+        subarraysCounter += 1;
+      }
+    }
+    if (subarraysCounter === 0) {
+      return 1;
+    }
+
+    let subarrayDepths = [];
+    for (let subarray of arr) {
+      if (Array.isArray(subarray)) {
+        subarrayDepths.push(this.calculateDepth(subarray));
+      }
+    }
+    
+    let max = subarrayDepths[0];
+    for (let i = 0; i < subarrayDepths.length; i++) {
+      if (max < subarrayDepths[i]) max = subarrayDepths[i];
+    }
+    return max + 1;
   }
 };
